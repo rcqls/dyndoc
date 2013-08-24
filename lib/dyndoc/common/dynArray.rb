13c62126c0b9a64c95e1772f8dc62b93963e2a08
R cqls <rdrouilh@gmail.com>
2013-08-24 12:04:48 +0200

    Array stuff added in dynArray.rb

--- lib/dyndoc/common/dynArray.rb
+++ lib/dyndoc/common/dynArray.rb
@@ -1,6 +1,76 @@
 require 'jl4rb'
 require 'R4rb'
 
+# Since more than one language is used for Array transfert
+# This stuff is moved from R4rb package to here
+
+class Array
+
+	def rb2R=(mode=nil)
+		##puts "rb2R mode #{object_id}";p mode
+		mode=R4rb unless mode
+		return if @rb2R_mode and @rb2R_mode==mode
+		@rb2R_mode=mode unless @rb2R_mode
+		@rb2R=(@rb2R_mode==Rserve ? Rserve::RVector.new("") :  R2rb::RVector.new("") )
+		##puts "rb2R=";p @rb2R
+	end
+
+	def rb2jl! #only one mode
+		@rb2jl= Julia::Vector.new("") 
+		##puts "rb2R=";p @rb2R
+	end
+	# out is a String representing a R or Julia expression 
+
+	def >(out)
+		out.strip!
+		mode=:r 
+		#out represents here an R or Julia object
+		if out =~ /^jl\:/
+			mode,out=:jl,out[3..-1]
+		elsif out =~ /^(r|R)\:/
+			out=out[2..-1]
+		end
+		case mode
+		when :r
+		    self.rb2R=nil unless @rb2R
+		    @rb2R << out
+		    @rb2R < self
+		when :jl
+			self.rb2jl=nil unless @rb2jl
+		    @rb2jl << out
+		    @rb2jl < self
+		end
+	    return self
+	end
+
+	def <(out) #out represents here an R or Julia expression to execute and put inside the Array
+	    out.strip!
+		mode=:r 
+		#out represents here an R or Julia object
+		if out =~ /^jl\:/
+			mode,out=:jl,out[3..-1]
+		elsif out =~ /^(r|R)\:/
+			out=out[2..-1]
+		end
+		case mode
+		when :r
+	    	self.rb2R=nil unless @rb2R
+	    	@rb2R << out
+	    	@rb2R > self
+	    when :jl
+	    	self.rb2jl! unless @rb2jl
+	    	@rb2jl << out
+	    	@rb2jl > self
+	    end
+	    	
+	    return self
+	end
+
+end
+
+
+## Dyndoc Array : ruby Array synchronized to Julia or R Vector
+# when change occurs from any language
 module Dyndoc
 	
 	class Vector
