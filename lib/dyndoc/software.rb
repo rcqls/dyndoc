bb976659111e31b8ebfa4966cdbb40efde9f47de
R cqls <rdrouilh@gmail.com>
2014-07-04 09:27:31 +0200

    pdflatex command with env

--- lib/dyndoc/software.rb
+++ lib/dyndoc/software.rb
@@ -60,6 +60,7 @@ module CqlsDoc
     unless SOFTWARE[:pdflatex]
       cmd=`type "pdflatex"`
       SOFTWARE[:pdflatex]=cmd.strip.split(" ")[2] unless cmd.empty?
+      SOFTWARE[:pdflatex] = "env TEXINPUTS=#{ENV['TEXINPUTS']} " + SOFTWARE[:pdflatex]
     end
     SOFTWARE[:pdflatex]
   end
